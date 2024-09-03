60e46c24a56949ddea7e8b81762461f00fd8847e
maxocean <39007967+haohaoget@users.noreply.github.com>
2024-09-03 12:54:30 +0800

    Update preset-clash-core.sh

--- N1/preset-clash-core.sh
+++ N1/preset-clash-core.sh
@@ -19,8 +19,8 @@ CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/maste
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
+#wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
+#wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
 wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
 wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
 wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
